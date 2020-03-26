eb4a39fb22b721ecc2f7be47d2e1aced33ea79ff
Javante Nelson <javantenelson@gmail.com>
2020-03-26 00:09:53 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+ if block_given?
+  num = 0
+  while num < array.length
+        yield array[num]
+        num = num + 1
+  end
+  array
+ end
 end
