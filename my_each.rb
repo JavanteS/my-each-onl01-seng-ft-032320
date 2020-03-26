fe04c1d0ad72c6d30fee9ad4e273e6c4475330d9
Javante Nelson <javantenelson@gmail.com>
2020-03-26 00:08:47 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,11 @@
 def my_each(array)
  if block_given?
  num = 0
- while num < array.length
+  while num < array.length
         yield array[num]
         num = num + 1
         
- end
+  end
  array
  end
 end
